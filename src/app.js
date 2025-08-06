fb5297784a2127fcbf4d50a82e1ef7dabec19025
Vincent0103 <vincentkanagasundaram@gmail.com>
2025-08-06 13:06:56 +0200

    Setup express and session based auth with passport.js and prisma

---
+++ src/app.js
@@ -0,0 +1,79 @@
+import { PrismaClient } from "@prisma/client";
+import { PrismaSessionStore } from "@quixo3/prisma-session-store";
+import express from "express";
+import expressSession from "express-session";
+import passport from "passport";
+import bcrypt from "bcryptjs";
+import { Strategy as LocalStrategy } from "passport-local";
+import path from "path";
+
+const app = express();
+const prisma = new PrismaClient();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(
+  expressSession({
+    cookie: {
+      maxAge: 7 * 24 * 60 * 60 * 1000, // ms
+    },
+    secret: "pochita",
+    resave: true,
+    saveUninitialized: true,
+    store: new PrismaSessionStore(new PrismaClient(), {
+      checkPeriod: 2 * 60 * 1000, // ms
+      dbRecordIdIsSessionId: true,
+      dbRecordIdFunction: undefined,
+    }),
+  }),
+);
+app.use(passport.session());
+app.use(express.urlencoded({ extended: true }));
+
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const user = await prisma.user.findUnique({
+        where: {
+          username,
+        },
+      });
+
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        return done(null, false, { message: "Incorrect password" });
+      }
+      return done(null, user);
+    } catch (err) {
+      done(err);
+    }
+  }),
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = prisma.user.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
+const { PORT } = process.env;
+app.listen(PORT, () => {
+  console.log(`Express app listening at port ${PORT}`);
+});
